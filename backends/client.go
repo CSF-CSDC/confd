277d09cac1096cafff87e4d8f00aadb22906dc3b
CSF-CSDC <407605174@qq.com>
2018-04-08 09:57:45 +0800

    Add case "sdc" in switch config.Backend in function New(config Config),which is defined in backends/client.go.

--- backends/client.go
+++ backends/client.go
@@ -3,19 +3,20 @@ package backends
 import (
 	"errors"
 	"strings"
-
-	"github.com/kelseyhightower/confd/backends/consul"
-	"github.com/kelseyhightower/confd/backends/dynamodb"
-	"github.com/kelseyhightower/confd/backends/env"
-	"github.com/kelseyhightower/confd/backends/etcd"
-	"github.com/kelseyhightower/confd/backends/etcdv3"
-	"github.com/kelseyhightower/confd/backends/file"
-	"github.com/kelseyhightower/confd/backends/rancher"
-	"github.com/kelseyhightower/confd/backends/redis"
-	"github.com/kelseyhightower/confd/backends/ssm"
-	"github.com/kelseyhightower/confd/backends/vault"
-	"github.com/kelseyhightower/confd/backends/zookeeper"
-	"github.com/kelseyhightower/confd/log"
+         
+        "confd/backends/sdc"
+	"confd/backends/consul"
+	"confd/backends/dynamodb"
+	"confd/backends/env"
+	"confd/backends/etcd"
+	"confd/backends/etcdv3"
+	"confd/backends/file"
+	"confd/backends/rancher"
+	"confd/backends/redis"
+	"confd/backends/ssm"
+	"confd/backends/vault"
+	"confd/backends/zookeeper"
+	"confd/log"
 )
 
 // The StoreClient interface is implemented by objects that can retrieve
@@ -33,7 +34,7 @@ func New(config Config) (StoreClient, error) {
 	backendNodes := config.BackendNodes
 
 	if config.Backend == "file" {
-		log.Info("Backend source(s) set to " + strings.Join(config.YAMLFile, ", "))
+		log.Info("Backend source(s) set to " + config.YAMLFile)
 	} else {
 		log.Info("Backend source(s) set to " + strings.Join(backendNodes, ", "))
 	}
@@ -58,23 +59,21 @@ func New(config Config) (StoreClient, error) {
 	case "rancher":
 		return rancher.NewRancherClient(backendNodes)
 	case "redis":
-		return redis.NewRedisClient(backendNodes, config.ClientKey, config.Separator)
+		return redis.NewRedisClient(backendNodes, config.ClientKey)
 	case "env":
 		return env.NewEnvClient()
 	case "file":
-		return file.NewFileClient(config.YAMLFile, config.Filter)
+		return file.NewFileClient(config.YAMLFile)
 	case "vault":
 		vaultConfig := map[string]string{
-			"app-id":    config.AppID,
-			"user-id":   config.UserID,
-			"role-id":   config.RoleID,
-			"secret-id": config.SecretID,
-			"username":  config.Username,
-			"password":  config.Password,
-			"token":     config.AuthToken,
-			"cert":      config.ClientCert,
-			"key":       config.ClientKey,
-			"caCert":    config.ClientCaKeys,
+			"app-id":   config.AppID,
+			"user-id":  config.UserID,
+			"username": config.Username,
+			"password": config.Password,
+			"token":    config.AuthToken,
+			"cert":     config.ClientCert,
+			"key":      config.ClientKey,
+			"caCert":   config.ClientCaKeys,
 		}
 		return vault.New(backendNodes[0], config.AuthType, vaultConfig)
 	case "dynamodb":
@@ -83,6 +82,8 @@ func New(config Config) (StoreClient, error) {
 		return dynamodb.NewDynamoDBClient(table)
 	case "ssm":
 		return ssm.New()
+        case "sdc":
+                return sdc.NewSdcClient(backendNodes, config.ClientCert, config.ClientKey, config.ClientCaKeys, config.BasicAuth, config.Username, config.Password)
 	}
 	return nil, errors.New("Invalid backend")
 }
